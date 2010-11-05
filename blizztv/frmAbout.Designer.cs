54e8a7962c892b5d2d6648f36d0ccce02d526f64
shalafiraistlin <shalafiraistlin@gmail.com>
2010-11-05 12:24:35 +0000

    Added LibStreams and LibVideoChannels project. Added proper assembly versioning support and about window now shows plugins information.

--- blizztv/frmAbout.Designer.cs
+++ blizztv/frmAbout.Designer.cs
@@ -40,6 +40,7 @@
             this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.buttonOK = new System.Windows.Forms.Button();
+            this.LinkLabelBlizzTV = new System.Windows.Forms.LinkLabel();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
@@ -49,7 +50,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Tahoma", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label1.Location = new System.Drawing.Point(334, 11);
+            this.label1.Location = new System.Drawing.Point(334, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(99, 33);
             this.label1.TabIndex = 5;
@@ -58,12 +59,13 @@
             // LinkLabelProjectPage
             // 
             this.LinkLabelProjectPage.AutoSize = true;
-            this.LinkLabelProjectPage.Location = new System.Drawing.Point(263, 44);
+            this.LinkLabelProjectPage.Location = new System.Drawing.Point(248, 113);
             this.LinkLabelProjectPage.Name = "LinkLabelProjectPage";
             this.LinkLabelProjectPage.Size = new System.Drawing.Size(170, 13);
             this.LinkLabelProjectPage.TabIndex = 6;
             this.LinkLabelProjectPage.TabStop = true;
             this.LinkLabelProjectPage.Text = "http://code.google.com/p/blizztv/";
+            this.LinkLabelProjectPage.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.LinkLabelProjectPage_LinkClicked);
             // 
             // pictureBox1
             // 
@@ -78,9 +80,10 @@
             // groupBox1
             // 
             this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.LinkLabelProjectPage);
             this.groupBox1.Location = new System.Drawing.Point(12, 63);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(421, 116);
+            this.groupBox1.Size = new System.Drawing.Size(421, 131);
             this.groupBox1.TabIndex = 8;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Copyleft 2010, BlizzTV project";
@@ -99,9 +102,9 @@
             // groupBox2
             // 
             this.groupBox2.Controls.Add(this.ListviewModules);
-            this.groupBox2.Location = new System.Drawing.Point(12, 185);
+            this.groupBox2.Location = new System.Drawing.Point(12, 200);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(421, 139);
+            this.groupBox2.Size = new System.Drawing.Size(421, 140);
             this.groupBox2.TabIndex = 9;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Plugins";
@@ -116,7 +119,7 @@
             this.ListviewModules.FullRowSelect = true;
             this.ListviewModules.Location = new System.Drawing.Point(3, 16);
             this.ListviewModules.Name = "ListviewModules";
-            this.ListviewModules.Size = new System.Drawing.Size(415, 120);
+            this.ListviewModules.Size = new System.Drawing.Size(415, 121);
             this.ListviewModules.TabIndex = 0;
             this.ListviewModules.UseCompatibleStateImageBehavior = false;
             this.ListviewModules.View = System.Windows.Forms.View.Details;
@@ -138,25 +141,36 @@
             // 
             // buttonOK
             // 
-            this.buttonOK.Location = new System.Drawing.Point(355, 330);
+            this.buttonOK.Location = new System.Drawing.Point(358, 346);
             this.buttonOK.Name = "buttonOK";
             this.buttonOK.Size = new System.Drawing.Size(75, 23);
-            this.buttonOK.TabIndex = 10;
+            this.buttonOK.TabIndex = 0;
             this.buttonOK.Text = "OK";
             this.buttonOK.UseVisualStyleBackColor = true;
             this.buttonOK.Click += new System.EventHandler(this.buttonOK_Click);
+            // 
+            // LinkLabelBlizzTV
+            // 
+            this.LinkLabelBlizzTV.AutoSize = true;
+            this.LinkLabelBlizzTV.Location = new System.Drawing.Point(316, 47);
+            this.LinkLabelBlizzTV.Name = "LinkLabelBlizzTV";
+            this.LinkLabelBlizzTV.Size = new System.Drawing.Size(117, 13);
+            this.LinkLabelBlizzTV.TabIndex = 11;
+            this.LinkLabelBlizzTV.TabStop = true;
+            this.LinkLabelBlizzTV.Text = "http://www.blizztv.com";
+            this.LinkLabelBlizzTV.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.LinkLabelBlizzTV_LinkClicked);
             // 
             // frmAbout
             // 
             this.AcceptButton = this.buttonOK;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(445, 361);
+            this.ClientSize = new System.Drawing.Size(445, 376);
+            this.Controls.Add(this.LinkLabelBlizzTV);
             this.Controls.Add(this.buttonOK);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.LinkLabelProjectPage);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -164,6 +178,7 @@
             this.MinimizeBox = false;
             this.Name = "frmAbout";
             this.Text = "About BlizzTV";
+            this.Load += new System.EventHandler(this.frmAbout_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
@@ -186,5 +201,6 @@
         private System.Windows.Forms.ColumnHeader columnHeader2;
         private System.Windows.Forms.ColumnHeader columnHeader3;
         private System.Windows.Forms.Button buttonOK;
+        private System.Windows.Forms.LinkLabel LinkLabelBlizzTV;
     }
 }
