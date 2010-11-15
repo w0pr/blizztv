05eb3b6fcdc70e5d564bbcaf6c48f1453630f286
shalafiraistlin <shalafiraistlin@gmail.com>
2010-11-15 09:58:57 +0000

    Changed all projects target platform to x86 because if we target AnyCPU or x64, the adobe flash player will resist working as it's a 32-bit dll (Issue 33). Implemented a basic dependency checker which will check for any required dependencies (and implemented flash player dependency).

---
+++ blizztv/DependencyChecker.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BlizzTV
+{
+    public class DependencyChecker:IDisposable
+    {
+        private bool disposed = false;
+
+        public DependencyChecker()
+        {
+            if (!this.CheckShockwaveFlash())
+            {
+                System.Windows.Forms.DialogResult result = System.Windows.Forms.MessageBox.Show("BlizzTV requires Abode Flash Player for internal video & stream playing and your system does not satisfy it. Do you want to install latest Adobe Flash Player now?", "Adobe Flash Player Missing!", System.Windows.Forms.MessageBoxButtons.YesNo, System.Windows.Forms.MessageBoxIcon.Error);
+                if (result == System.Windows.Forms.DialogResult.Yes) System.Diagnostics.Process.Start("IExplore.exe", "http://get.adobe.com/flashplayer/");
+            }
+        }
+
+        private bool CheckShockwaveFlash()
+        {
+            object flash_object;
+            bool satisfied = true;            
+            try
+            {
+                flash_object = Activator.CreateInstance(Type.GetTypeFromProgID("ShockwaveFlash.ShockwaveFlash"));
+            }
+            catch (Exception e)
+            {
+                satisfied = false;
+            }
+            finally
+            {
+                flash_object = null;
+            }
+            return satisfied;
+        }
+
+        ~DependencyChecker() { Dispose(false); }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing) { }
+                disposed = true;
+            }
+        }
+    }       
+}
