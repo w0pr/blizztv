795284e177a15c97d0dd1074517c7d30f20e006d
shalafiraistlin <shalafiraistlin@gmail.com>
2010-12-01 11:18:04 +0000

    Re-applied svn-keywords to *.cs files as our folder structure changed a lot.

--- blizztv/Modules/Videos/Providers.cs
+++ blizztv/Modules/Videos/Providers.cs
@@ -12,7 +12,7 @@
  * You should have received a copy of the GNU General Public License along with this program.  If not, see 
  * <http://www.gnu.org/licenses/>. 
  * 
- * $Id: Providers.cs 154 2010-11-29 14:46:54Z shalafiraistlin@gmail.com $
+ * $Id$
  */
 
 using System;
