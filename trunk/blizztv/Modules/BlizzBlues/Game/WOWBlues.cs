4e1f8ae0f3879959cd2cebddc881b0fd099d3703
shalafiraistlin@gmail.com <shalafiraistlin@gmail.com@728a86d0-6b09-3f8a-e661-e28d8fc551a4>
2010-12-10 13:27:38 +0000

    Started implementing BlizzBlues plugin

---
+++ trunk/blizztv/Modules/BlizzBlues/Game/WOWBlues.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BlizzTV.Modules.BlizzBlues.Game
+{
+    public class WOWBlues:BlueParser
+    {
+        public WOWBlues()
+            : base("World of Warcraft")
+        {
+            this.Sources = new BlueSource[2] { new BlueSource(Region.Us, "http://us.battle.net/wow/en/forum/blizztracker/"), new BlueSource(Region.Eu, "http://eu.battle.net/wow/en/forum/blizztracker/"), };
+            this.Parse(); 
+        }
+    }
+}
