124eca60d129db0fa16e3320cd68e9ea6337a881
shalafiraistlin@gmail.com <shalafiraistlin@gmail.com@728a86d0-6b09-3f8a-e661-e28d8fc551a4>
2010-12-23 17:53:34 +0000

    Implemented Registery helper. Implemented SystemStartup helper. Added startup on system startup option.

---
+++ trunk/blizztv/CommonLib/Helpers/Registry.cs
@@ -0,0 +1,98 @@
+﻿/*    
+ * Copyright (C) 2010, BlizzTV Project - http://code.google.com/p/blizztv/
+ *  
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General 
+ * Public License as published by the Free Software Foundation, either version 3 of the License, or (at your 
+ * option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the 
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License 
+ * for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with this program.  If not, see 
+ * <http://www.gnu.org/licenses/>. 
+ * 
+ * $Id$
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Win32;
+
+namespace BlizzTV.CommonLib.Helpers
+{
+    public enum RootKey
+    {
+        HKEY_CLASSES_ROOT,
+        HKEY_CURRENT_USER,
+        HKEY_LOCAL_MACHINE
+    }
+
+    public static class Registry
+    {
+        public static RegistryKey GetKey(RootKey rootKey, string key)
+        {
+            RegistryKey root = null;
+            switch (rootKey)
+            {
+                case RootKey.HKEY_CLASSES_ROOT: root = Microsoft.Win32.Registry.ClassesRoot; break;
+                case RootKey.HKEY_CURRENT_USER: root = Microsoft.Win32.Registry.CurrentUser; break;
+                case RootKey.HKEY_LOCAL_MACHINE: root = Microsoft.Win32.Registry.LocalMachine;break;
+            }
+
+            return root.OpenSubKey(key, true);
+        }
+
+        public static object GetValue(RootKey rootKey, string key, string valueName)
+        {
+            if (!KeyExists(rootKey, key)) return null;
+            RegistryKey askedKey = GetKey(rootKey, key);
+            object value = askedKey.GetValue(valueName, null);
+            askedKey.Close();
+            askedKey.Flush();
+            return value;
+        }
+
+        public static bool SetValue(RootKey rootKey, string key, string valueName, object value)
+        {
+            RegistryKey askedKey = GetKey(rootKey, key);
+            if (askedKey == null) return false;
+            askedKey.SetValue(valueName, value);
+            askedKey.Close();
+            askedKey.Flush();
+            return true;
+        }
+
+        public static void DeleteValue(RootKey rootKey, string key, string valueName)
+        {
+            if (!ValueExists(rootKey, key, valueName)) return;
+            RegistryKey askedKey = GetKey(rootKey, key);
+            askedKey.DeleteValue(valueName);
+            askedKey.Close();
+            askedKey.Flush();
+        }
+
+        public static bool KeyExists(RootKey rootKey, string key)
+        {
+            RegistryKey askedKey = GetKey(rootKey, key);
+            if (askedKey == null) return false;
+            askedKey.Close();
+            askedKey.Flush();
+            return true;
+        }
+
+        public static bool ValueExists(RootKey rootKey, string key, string valueName)
+        {
+            if (!KeyExists(rootKey, key)) return false;
+            RegistryKey askedKey = GetKey(rootKey, key);
+            object value = askedKey.GetValue(valueName, null);
+            askedKey.Close();
+            askedKey.Flush();
+            if (value != null) return true; else return false;
+        }
+
+        
+    }
+}
---
+++ trunk/blizztv/CommonLib/Helpers/SystemStartup.cs
@@ -0,0 +1,52 @@
+﻿/*    
+ * Copyright (C) 2010, BlizzTV Project - http://code.google.com/p/blizztv/
+ *  
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General 
+ * Public License as published by the Free Software Foundation, either version 3 of the License, or (at your 
+ * option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the 
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License 
+ * for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with this program.  If not, see 
+ * <http://www.gnu.org/licenses/>. 
+ * 
+ * $Id$
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+
+namespace BlizzTV.CommonLib.Helpers
+{
+    public static class SystemStartup
+    {
+        private const string _startupKey = "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run";
+
+        public static bool Enabled
+        {
+            get
+            {
+                string expectedValue = string.Format("\"{0}\"", Assembly.GetExecutingAssembly().Location);
+                if (!Registry.ValueExists(RootKey.HKEY_LOCAL_MACHINE, _startupKey, "BlizzTV") || (string)Registry.GetValue(RootKey.HKEY_LOCAL_MACHINE, _startupKey, "BlizzTV") != expectedValue) return false;
+                return true;
+            }
+            set
+            {
+                if (value == true) // if the system startup value does not exist or is path is incorrect, reset the registry value.
+                {
+                    string pathValue = string.Format("\"{0}\"", Assembly.GetExecutingAssembly().Location);
+                    if (!Registry.ValueExists(RootKey.HKEY_LOCAL_MACHINE, _startupKey, "BlizzTV") || (string)Registry.GetValue(RootKey.HKEY_LOCAL_MACHINE, _startupKey, "BlizzTV") != pathValue) Registry.SetValue(RootKey.HKEY_LOCAL_MACHINE, _startupKey, "BlizzTV", pathValue);
+                }
+                else // if the system startup value exists, delete it.
+                {
+                    if (Registry.ValueExists(RootKey.HKEY_LOCAL_MACHINE, _startupKey, "BlizzTV")) Registry.DeleteValue(RootKey.HKEY_LOCAL_MACHINE, _startupKey, "BlizzTV"); 
+                }
+            }
+        }
+    }
+}
