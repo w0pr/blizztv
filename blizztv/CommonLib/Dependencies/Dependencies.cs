ea47b64ea1909e3a997c157478797b6e4bd63e29
shalafiraistlin <shalafiraistlin@gmail.com>
2010-12-31 09:56:35 +0000

    Implemented a dependency-checker rule for installing Microsoft Visual C++ 2010 Redistributable Package (x86)  on missing systems (Issue125). The run-time is required by the irrKlang engine .

--- blizztv/CommonLib/Dependencies/Dependencies.cs
+++ blizztv/CommonLib/Dependencies/Dependencies.cs
@@ -20,6 +20,7 @@ using System.Diagnostics;
 using System.Windows.Forms;
 using BlizzTV.CommonLib.Logger;
 using BlizzTV.CommonLib.Downloads;
+using BlizzTV.CommonLib.Audio;
 
 namespace BlizzTV.CommonLib.Dependencies
 {
@@ -39,7 +40,7 @@ namespace BlizzTV.CommonLib.Dependencies
             if (!ShockwaveFlashInstalled())
             {
                 System.Windows.Forms.DialogResult result = MessageBox.Show("BlizzTV requires Abode Flash Player for best user-experience. Do you want to install latest Adobe Flash Player now?", "Missing component: Adobe Flash Player", System.Windows.Forms.MessageBoxButtons.YesNo, System.Windows.Forms.MessageBoxIcon.Exclamation);
-                if (result == System.Windows.Forms.DialogResult.Yes)
+                if (result == DialogResult.Yes)
                 {
                     frmDownload f = new frmDownload("Downloading latest Adobe Flash player");
                     f.StartDownload(new Download("http://fpdownload.adobe.com/get/flashplayer/current/install_flash_player_ax.exe", "install_flash_player_ax.exe"));
@@ -52,11 +53,46 @@ namespace BlizzTV.CommonLib.Dependencies
                     {
                         MessageBox.Show("Adobe Flash Player download failed. Please install it manually.", "Download Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
                         Process.Start("IExplore.exe", "http://get.adobe.com/flashplayer/");
-                    }
-                    return false;
+                    }                    
                 }
+                return false;
+            }
+            else if (!VisualCPP2010RuntimeInstalled())
+            {
+                System.Windows.Forms.DialogResult result = MessageBox.Show("BlizzTV requires Microsoft Visual C++ 2010 redistributable package. Do you want to install it now?", "Missing component: Microsoft Visual C++ 2010 Redistributable Package", System.Windows.Forms.MessageBoxButtons.YesNo, System.Windows.Forms.MessageBoxIcon.Exclamation);
+                if (result == DialogResult.Yes)
+                {
+                    frmDownload f = new frmDownload("Downloading latest Microsoft Visual C++ 2010 redistributable package");
+                    f.StartDownload(new Download("http://download.microsoft.com/download/5/B/C/5BC5DBB3-652D-4DCE-B14A-475AB85EEF6E/vcredist_x86.exe", "vcredist_x86.exe"));
+                    if (f.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                    {
+                        MessageBox.Show("The application will now exit to continue installation of Microsoft Visual C++ 2010 redistributable package. Please restart BlizzTV after installation finishes.", "Download Complete", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        Process.Start("vcredist_x86.exe");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Microsoft Visual C++ 2010 redistributable download failed. Please install it manually.", "Download Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        Process.Start("IExplore.exe", "http://www.microsoft.com/downloads/en/details.aspx?familyid=A7B7A05E-6DE6-4D3A-A423-37BF0912DB84&displaylang=en");
+                    }                    
+                }
+                return false;
             }
             return true;
+        }
+
+        private bool VisualCPP2010RuntimeInstalled()
+        {
+            bool satisfied = true;
+            try
+            {
+                AudioPlayer player = AudioPlayer.Instance;
+            }
+            catch (Exception e)
+            {
+                satisfied = false;
+                Log.Instance.Write(LogMessageTypes.Error, string.Format("Dependency VisualCPP2010 Runtime not satisfied: {0}", e));
+            }
+            return satisfied;
         }
 
         private bool ShockwaveFlashInstalled()
@@ -69,7 +105,7 @@ namespace BlizzTV.CommonLib.Dependencies
             catch (Exception e)
             {
                 satisfied = false;
-                Log.Instance.Write(LogMessageTypes.Error, string.Format("CheckShockwaveFlash Exception: {0}", e));
+                Log.Instance.Write(LogMessageTypes.Error, string.Format("Dependency Shockwave Flash not satisfied: {0}", e));
             }
             return satisfied;
         }
